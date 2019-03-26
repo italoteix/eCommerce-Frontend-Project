150a1a9d6e0481d4dc79ed9b3eabd50ed02ff95e
Ítalo Teixeira <italo_bruno91@hotmail.com>
2019-03-26 00:09:17 -0300

    finish product.html

--- js/index.js
+++ js/index.js
@@ -196,3 +196,35 @@ $(function () {
 
     cycle();
 });
+
+
+
+// Dropdown Buttons text =======================================================
+
+$(".dropdown-menu a").click(function(){
+  $(this).parents(".dropdown").find('.btn-cat').html($(this).text() + ' <i class="flaticon-down-arrow"></i>');
+  $(this).parents(".dropdown").find('.btn-cat').val($(this).data('value'));
+
+  $(this).parents(".dropdown").find('.show-all').html($(this).text() + ' <i class="flaticon-down-arrow"></i>');
+  $(this).parents(".dropdown").find('.show-all').val($(this).data('value'));
+});
+
+
+// Grid / List view ==============================================================
+
+$(document).ready(function() {
+  
+  $('#btn-list').click(function(event){
+    event.preventDefault();
+    $('.grid').addClass('list');
+    $('#btn-grid').removeClass('btn-view');
+    $('#btn-list').addClass('btn-view');
+  });
+
+  $('#btn-grid').click(function(event){
+    event.preventDefault();
+    $('.grid').removeClass('list');
+    $('#btn-grid').addClass('btn-view');
+    $('#btn-list').removeClass('btn-view');
+  });
+});
